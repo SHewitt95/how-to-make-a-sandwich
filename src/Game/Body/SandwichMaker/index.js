66946373a041a0d05cfcfe7afc2769ede005a119
Sherman Hewitt <hewitt.sherman@gmail.com>
2019-11-10 18:11:54 -0500

    Kitchen to Supplies, check for only bread count

--- src/Game/Body/SandwichMaker/index.js
+++ src/Game/Body/SandwichMaker/index.js
@@ -1,15 +1,14 @@
 import React, { useContext } from 'react';
 import GenericItem from './Supplies/GenericItem';
 import { BASE_PRICES } from '../_utils/constants';
-import { haveEnoughInventory } from '../_utils/helpers';
 import { Items, Context, Actions } from '../../context/GlobalState';
 
 const SandwichMaker = () => {
   const [state, dispatch] = useContext(Context);
   return (
     <>
-      <h4>Kitchen</h4>
-      <button disabled={!haveEnoughInventory(state)} onClick={() => dispatch({ type: Actions.MAKE_SANDWICH })}>Make Sandwich</button>
+      <h4>Supplies</h4>
+      <button disabled={state.supply[Items.BREAD].inventory === 0} onClick={() => dispatch({ type: Actions.MAKE_SANDWICH })}>Make Sandwich</button>
       <GenericItem itemName={Items.BREAD} refillPrice={BASE_PRICES[Items.BREAD].refill} upgradePrice={BASE_PRICES[Items.BREAD].upgrade} />
       <GenericItem itemName={Items.CONDIMENTS} refillPrice={BASE_PRICES[Items.CONDIMENTS].refill} upgradePrice={BASE_PRICES[Items.CONDIMENTS].upgrade} />
       <GenericItem itemName={Items.VEGETABLES} refillPrice={BASE_PRICES[Items.VEGETABLES].refill} upgradePrice={BASE_PRICES[Items.VEGETABLES].upgrade} unlockPrice={BASE_PRICES[Items.VEGETABLES].unlock} />
