5f899fd32e635ce69e3caf24053e85f02ea1e674
Sherman Hewitt <hewitt.sherman@gmail.com>
2019-11-10 18:11:31 -0500

    change page title

--- src/Game/Body/SandwichCounter/index.js
+++ src/Game/Body/SandwichCounter/index.js
@@ -1,8 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Context } from '../../context/GlobalState';
 
 const SandwichCounter = () => {
   const [state] = useContext(Context);
+
+  useEffect(() => {
+    document.title = `${state.sandwichCount} sandwich${state.sandwichCount === 1 ? '' : 'es'} - How To Make A Sandwich`;
+  }, [state.sandwichCount]);
+
   return (
     <h3>{`Sandwiches: ${state.sandwichCount}`}</h3>
   );
