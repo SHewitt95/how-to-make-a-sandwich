e41389cf85a8fd0ccd90573aaa066b72db8cbef0
Sherman Hewitt <hewitt.sherman@gmail.com>
2019-11-10 18:11:17 -0500

    should I consider SandwichList...

--- src/Game/Body/index.js
+++ src/Game/Body/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SandwichCounter from './SandwichCounter';
 import SandwichMaker from './SandwichMaker';
+// import SandwichList from './SandwichList';
 
 const Body = () => {
   return (
