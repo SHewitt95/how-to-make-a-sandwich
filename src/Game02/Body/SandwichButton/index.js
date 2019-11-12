738e61db4b1f00dfa0358909041d3241724376b8
SHewitt95 <hewitt.sherman@gmail.com>
2019-11-11 22:44:44 -0500

    no cheating! ;)

--- src/Game02/Body/SandwichButton/index.js
+++ src/Game02/Body/SandwichButton/index.js
@@ -2,10 +2,17 @@ import React, { useContext } from 'react';
 import Context from '../../data/context';
 import { Actions } from '../../data/_utils/constants';
 
+const disableHoldingEnter = event => {
+  // No cheating! ;)
+  if (event.key === 'Enter'){
+    event.preventDefault();
+  }
+}
+
 const SandwichButton = () => {
   const [, dispatch] = useContext(Context);
   return (
-    <button onClick={() => dispatch({ type: Actions.MAKE_SANDWICH })}>Make Sandwich</button>
+    <button onClick={() => dispatch({ type: Actions.MAKE_SANDWICH })} onKeyDown={disableHoldingEnter}>Make Sandwich</button>
   );
 }
 
