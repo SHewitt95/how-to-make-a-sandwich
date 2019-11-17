1e7d11a3ba7a32feb04626fe1a3459adbef5582f
SHewitt95 <hewitt.sherman@gmail.com>
2019-11-17 12:49:38 -0500

    add states, cooldown to menu item upgrades

--- src/Game02/Body/SandwichMenu/MenuItem/index.js
+++ src/Game02/Body/SandwichMenu/MenuItem/index.js
@@ -1,12 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Context from '../../../data/context';
 import { Actions } from '../../../data/_utils/constants';
 
-const MenuItem = ({ itemName }) => {
+const STATES = {
+  IDLE: 'idle',
+  COOLDOWN: 'cooldown',
+};
+
+const MenuItem = ({ itemName, cooldownTime }) => {
   const [, dispatch] = useContext(Context);
+  const [state, setState] = useState(STATES.IDLE);
+
+  // https://upmostly.com/tutorials/settimeout-in-react-components-using-hooks
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setState(STATES.IDLE);
+    }, cooldownTime);
+    return () => clearTimeout(timer);
+  }, [cooldownTime, state]);
+
   return (
     <li>
-      <button onClick={() => dispatch({ type: Actions.UPGRADE_MENU_ITEM, payload: 1, itemName })}>
+      <button disabled={state === STATES.COOLDOWN} onClick={() => {
+        dispatch({ type: Actions.UPGRADE_MENU_ITEM, payload: 1, itemName });
+        setState(STATES.COOLDOWN);
+      }}>
         {itemName}
       </button>
     </li>
--- src/Game02/Body/SandwichMenu/index.js
+++ src/Game02/Body/SandwichMenu/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../../data/context';
 import MenuItem from './MenuItem';
 import { Menu } from '../../data/_utils/constants'
 
+const COOLDOWN_TIMES = {
+  Bread: 4000,
+  Condiments: 10000,
+  Cheese: 16000,
+  Vegetables: 24000,
+  Meat: 40000,
+}
+
+const UNLOCK_LEVEL = {
+  Bread: 0,
+  Condiments: 5,
+  Cheese: 10,
+  Vegetables: 20,
+  Meat: 30,
+}
 
 const SandwichMenu = () => {
+  const [state] = useContext(Context);
+  const { playerLevel } = state;
   return (
     <ol>
-      {Object.keys(Menu).map(name => <MenuItem key={`SandwichMenu_${Menu[name]}`} itemName={Menu[name]} />)}
+      {Object.keys(Menu).map(name => {
+        const item = Menu[name];
+        const itemInState = state.menuItems[item]
+        if (playerLevel < UNLOCK_LEVEL[item]) return null;
+        return (
+          <MenuItem cooldownTime={COOLDOWN_TIMES[item] * (1.25 * itemInState.level)} key={`SandwichMenu_${item}`} itemName={item} />
+        );
+      })}
     </ol>
   );
 };
