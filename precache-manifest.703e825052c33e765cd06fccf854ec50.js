f46cbbffab1ed1016fedc9503928ac5b9867ebb4
Sherman Hewitt <hewitt.sherman@gmail.com>
2019-11-17 18:08:02 -0500

    Updates

---
+++ precache-manifest.703e825052c33e765cd06fccf854ec50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0df29e7792b216efade8fbe72036e41",
+    "url": "/how-to-make-a-sandwich-game/index.html"
+  },
+  {
+    "revision": "40276765a1fb2a93fcad",
+    "url": "/how-to-make-a-sandwich-game/static/css/main.89352419.chunk.css"
+  },
+  {
+    "revision": "e1f90f2bacde7ef334d1",
+    "url": "/how-to-make-a-sandwich-game/static/js/2.b579ec66.chunk.js"
+  },
+  {
+    "revision": "40276765a1fb2a93fcad",
+    "url": "/how-to-make-a-sandwich-game/static/js/main.e15aae34.chunk.js"
+  },
+  {
+    "revision": "e65659c0ca01d0e91fb5",
+    "url": "/how-to-make-a-sandwich-game/static/js/runtime-main.9287ab92.js"
+  }
+]);
