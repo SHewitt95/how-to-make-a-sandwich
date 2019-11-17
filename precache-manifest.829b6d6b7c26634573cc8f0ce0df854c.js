1f9cb2df43a55da7907baa7792bd3942936bfa67
Sherman Hewitt <hewitt.sherman@gmail.com>
2019-11-17 18:42:03 -0500

    Updates

---
+++ precache-manifest.829b6d6b7c26634573cc8f0ce0df854c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853d0e6f326d0102e003c6321f6d2436",
+    "url": "/how-to-make-a-sandwich/index.html"
+  },
+  {
+    "revision": "750a1525dcd201028263",
+    "url": "/how-to-make-a-sandwich/static/css/main.89352419.chunk.css"
+  },
+  {
+    "revision": "e1f90f2bacde7ef334d1",
+    "url": "/how-to-make-a-sandwich/static/js/2.b579ec66.chunk.js"
+  },
+  {
+    "revision": "750a1525dcd201028263",
+    "url": "/how-to-make-a-sandwich/static/js/main.f863449f.chunk.js"
+  },
+  {
+    "revision": "0eafa5fa1f231187b292",
+    "url": "/how-to-make-a-sandwich/static/js/runtime-main.0cfef1eb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/how-to-make-a-sandwich-game/precache-manifest.703e825052c33e765cd06fccf854ec50.js"
+  "/how-to-make-a-sandwich/precache-manifest.829b6d6b7c26634573cc8f0ce0df854c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/how-to-make-a-sandwich-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/how-to-make-a-sandwich/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
